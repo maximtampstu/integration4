c72794d070fdb78f33a5b567bd1060e89e351115
Maxim Tampere <maxim.tampere@student.howest.be>
2025-06-05 16:13:18 +0200

    theme voting logic added

--- client-website/app/layouts/BaseLayout.jsx
+++ client-website/app/layouts/BaseLayout.jsx
@@ -1,11 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
 
-export const clientLoader = async () => {
-    console.log("test3");
-    // handle form logic here
-};
-
 const BaseLayout = () => (
     <>
         <NavBar />
--- client-website/app/routes/vote-theme.jsx
+++ client-website/app/routes/vote-theme.jsx
@@ -1,15 +1,133 @@
-import { Link } from "react-router";
+import { Link, useFetcher } from "react-router";
+import { getThemeVotes, addThemeVote } from "../services/theme";
+import { getCurrent } from "../services/current";
+import { getUserById } from "../services/users";
+import { useState, useEffect } from "react";
 
-const VoteTheme = () => {
+export async function clientLoader() {
+  const current = await getCurrent();
+  const votebleThemes = current.themes;
+  const themeVotes = await getThemeVotes();
+  const email = (await getUserById(current.userId)).email
+  return { votebleThemes, themeVotes, email };
+}
 
+export async function clientAction({ request }) {
+  const data = await request.formData()
+  const themeId = data.get("themeId")
+  const email = data.get("email")
+  await addThemeVote(themeId, email)
+}
+
+const VoteTheme = ({ loaderData }) => {
+  const { votebleThemes, themeVotes, email } = loaderData;
+  const fetcher = useFetcher();
+
+  const [voted, setVoted] = useState(false)
+  const [themeId, setThemeId] = useState("")
+  const [hasAlreadyVoted, setHasAlreadyVoted] = useState(false)
+
+  //for this i used chatGPT because i could not find a other way how i could do it after to long trying
+  useEffect(() => {
+    const already = themeVotes.some((vote) => vote.userEmail === email);
+    setHasAlreadyVoted(already);
+  }, [themeVotes, email]);
+
+  const handleSubmitVote =  (e) =>{
+    e.preventDefault();
+    setThemeId(parseInt(e.target.theme.value))
+    fetcher.submit({ email, themeId: e.target.theme.value }, { method: "post" });
+    if (fetcher.state === "idle") {
+      setVoted(true);
+    }
+  }
+
+  if (hasAlreadyVoted && !voted) {
     return (
-        <>
-            <h1>Vote Theme</h1>
-            <ul>
-                <li><Link to="/participate">Back</Link></li>
-            </ul>
-        </>
+      <>
+        <h1>Vote Theme</h1>
+        <Link to="/participate">Back</Link>
+        <p>You already voted.</p>
+      </>
     );
+  }
+
+  return (
+    <>
+      <h1>Vote Theme</h1>
+      <Link to="/participate">Back</Link>
+      {!voted ? (
+        <form onSubmit={handleSubmitVote}>
+          <input type="hidden" value={email} />
+          {votebleThemes.map(theme => <label key={theme.id}><input type="radio" name="theme" value={theme.id} required />{theme.name}</label>)}
+          <button type="submit">Vote</button>
+          <p>{fetcher.state}</p>
+        </form>
+      ) : (
+          <p>Thanks for voting. You voted for <b>{((votebleThemes.find(item => item.id === themeId).name))}</b></p>
+      )}
+    </>
+  );
 };
 
 export default VoteTheme;
+
+
+/*
+import { Link, redirect } from "react-router";
+import { getThemeVotes, addThemeVote } from "../services/theme";
+import { getCurrent } from "../services/current";
+import VoteVerification from "../components/VoteVerification/VoteVerification";
+import { useState } from "react";
+
+export async function clientLoader() {
+  const current = await getCurrent();
+  const themeVotes = await getThemeVotes();
+  return { current, themeVotes };
+}
+
+export async function clientAction({ request }) {
+  const data = await request.formData()
+  const themeId = data.get("themeId")
+  const email = data.get("email")
+  await addThemeVote(themeId, email)
+  return redirect(`/vote-complete/${themeId}`)
+}
+
+const VoteTheme = ({ loaderData }) => {
+  const { current, themeVotes } = loaderData;
+  const votebleThemes = current.themes
+
+  const [state, setState] = useState("email")
+  const [vote, setVote] = useState("")
+
+  const changeState = (state) => {
+    setState(state)
+  }
+  
+  const handleSubmitVote = (e) => {
+    e.preventDefault()
+    setVote(e.target.theme.value)
+  }
+
+  const ClosePopup = () => {
+    setState("email")
+    setVote("")
+  }
+
+  return (
+    <>
+      <h1>Vote Theme</h1>
+        <Link to="/vote">Back</Link>
+        <p>Vote Theme</p>
+        <form onSubmit={handleSubmitVote}>
+          {votebleThemes.map(theme => <label key={theme.id}><input type="radio" name="theme" value={theme.id} required />{theme.name}</label>)}
+          <button type="submit">Vote</button>
+        </form>
+      {vote != "" ? <VoteVerification themeVotes={themeVotes} state={state} changeState={changeState} themeId={vote} ClosePopup={ClosePopup} /> : null}
+    </>
+  );
+};
+
+export default VoteTheme;
+*/
