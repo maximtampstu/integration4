bf38d37bf5cce0d05f7987c31605a4ccbabc710d
Maxim Tampere <maxim.tampere@student.howest.be>
2025-06-15 10:11:32 +0200

    vote them page for the kiosk html, css and working

--- app/components/VoteVerification/VoteVerification.jsx
+++ app/components/VoteVerification/VoteVerification.jsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Link, Form, useFetcher } from "react-router";
 import emailjs from 'emailjs-com';
+import "./VoteVerification.css"
 
-const VoteVerification = ({ state, themeVotes, changeState, themeId, ClosePopup }) => {
+const VoteVerification = ({ state, themeVotes, changeState, themeId, ClosePopup, votableThemes }) => {
     const addresses = ["@gmail.com", "@outlook.com", "@icloud.com", "@yahoo.com", "@hotmail.com", "@telenet.be", "@mail.com"]
     const fetcher = useFetcher();
+    const inputRef = useRef(null);
 
     const [email, setEmail] = useState("")
-    const [error, setError] = useState("")
+    const [error, setError] = useState("/")
     const [code, setCode] = useState("")
     const [verifyCode, setVerifyCode] = useState("")
 
     const handleClickBack = (type) => {
         setEmail("")
-        setError("")
+        setError("/")
         setCode("")
         setVerifyCode("")
         if(type === "close"){
@@ -26,12 +28,19 @@ const VoteVerification = ({ state, themeVotes, changeState, themeId, ClosePopup 
 
     const handleSubmitEmail = (e) => {
         e.preventDefault()
-        const hasVoted = themeVotes.some(vote => vote.userEmail === email);
+
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+            setError("Email requierd")
+            return;
+        }
+        
+        const hasVoted = themeVotes.some(vote => vote.userEmail === email.toLowerCase());
 
         if(hasVoted){
             setError("You already have voted")
+            setEmail("")
         } else {
-            setError("")
+            setError("/")
             const generatedCode = Math.floor(100000 + Math.random() * 900000).toString();
             setCode(generatedCode)
             changeState("code")
@@ -50,51 +59,90 @@ const VoteVerification = ({ state, themeVotes, changeState, themeId, ClosePopup 
             },
             'Satxo3KS8iiEr06Bj' // public key from EmailJS
         )
-            //for sending the mail I used chatGPT becaus I could not find how I needed to send the mail via code
-            .then((result) => {
-                console.log("Email sent:", result.text);
-            }, (error) => {
-                console.log("Failed to send:", error.text);
-            });
+        //for sending the mail I used chatGPT becaus I could not find how I needed to send the mail via code
+        .then((result) => {
+            console.log("Email sent:", result.text);
+        }, (error) => {
+            console.log("Failed to send:", error.text);
+        });
         
+    };
+
+    const handleCodeChange = (e) => {
+        const value = e.target.value.replace(/\D/g, '').slice(0, 6);
+        setVerifyCode(value);
     };
 
     const handleSubmitVerifyCode = (e) => {
         e.preventDefault()
-        if(verifyCode === code){
+        if (verifyCode === ""){
+            setError("Pleas fill in your verification code")
+        } else if (verifyCode.length !== 6){
+            setError("This code contains 6 digits")
+        } else if(verifyCode === code){
             fetcher.submit({ email, themeId }, { method: "post" });
         } else {
-            setError("wrong")
+            setError("The code you entered is incorrect")
         }
     }
 
     return (
-        <div style={{ padding: "1rem", background: "#eee", width: "max-content" }}>
-            <button type="button" onClick={() => handleClickBack("close")} >X</button>
-            {state === "email" ? (
-                <form onSubmit={handleSubmitEmail}>
-                    <label>email<input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="john.doe@gmail.com" required /></label>
-                    <br />
-                    {error}
-                    <br />
-                    {addresses.map(addres => <button type="button" key={addres} onClick={() => setEmail(email + addres)} >{addres}</button>)}
-                    <br />
-                    <button type="button" onClick={() => setEmail("")} >Clear</button>
-                    <br />
-                    <button type="button" onClick={() => handleClickBack("close")} >Cancel</button>
-                    <button type="submit">Send Code</button>
-                </form>
-            ) : (
-                <form onSubmit={handleSubmitVerifyCode}>
-                    <label>code<input type="text" inputMode="numeric" pattern="[0-9]*" name="verifyCode" minLength={6} maxLength={6} value={verifyCode} onChange={(e) => /^\d*$/.test(e.target.value) && setVerifyCode(e.target.value)} placeholder="######" required/></label>
-                    <br />
-                    <button type="button" onClick={() => sendVerificationEmail(email, generatedCode)} >Resent code</button>
-                    {error}
-                    <br />
-                        <button type="button" onClick={() => handleClickBack("back")} >Change Email</button>
-                    <button type="submit">Verify</button>
-                </form>
-            )}
+        <div onClick={() => handleClickBack("close")}  className="vote-verification">
+            <div onClick={(e) => e.stopPropagation()} className="vote-verification__content">
+                <button type="button" className="kiosk-button kiosk-button--sec kiosk-button--small" onClick={() => handleClickBack("close")} >X</button>
+                {state === "email" ? (
+                    <form className="vote-email" onSubmit={handleSubmitEmail}>
+                        <div className="vote-email__head">
+                            <div className="vote-email__choice">
+                                <p>You picked</p>
+                                <h3>{votableThemes.find(theme => theme.id === Number(themeId)).name}</h3>
+                            </div>
+                            <div className="vote-email__extra">
+                                <p className="vote-email__info">Enter your email to confirm your vote</p>
+                                <p className="vote-email__why">*We ask for your email to avoid spam and make sure every vote counts from a real person, like you!</p>
+                            </div>
+                        </div>
+                        <div className="vote-email__content">
+                            <label><div><span>Email</span><span style={{ color: error === "/" ? "transparent" : "var(--color-lau-yellow)"}} className="vote-email__error">{error}</span></div><input type="text" name="email" value={email} onChange={(e) => {setEmail(e.target.value); setError("/")}} placeholder="john.doe@gmail.com" /></label>
+                            <ul>
+                                {addresses.map(addres => <li><button type="button" key={addres} onClick={() => setEmail(email + addres)} >{addres}</button></li>)}
+                                <li><button className="vote-email__clear" type="button" onClick={() => setEmail("")} >Clear</button></li>
+                            </ul>
+                            <div className="vote-verification__buttons">
+                                <button className="kiosk-button kiosk-button--sec" type="button" onClick={() => handleClickBack("close")} >Cancel</button>
+                                <button className="kiosk-button"type="submit">Send Code</button>
+                            </div>
+                        </div>
+                    </form>
+                ) : (
+                    <form className="vote-code" onSubmit={handleSubmitVerifyCode}>
+                        <div className="vote-code__top">
+                            <div className="vote-code__head">
+                                <h3>Verify your email</h3>
+                                <p>We sent a 6-digit code to <span>{email}</span> Enter it below to confirm your vote.</p>
+                            </div>
+                            <div className="vote-code__mid">
+                                <div>
+                                    <input type="text" ref={inputRef} inputMode="numeric" autoComplete="one-time-code" pattern="[0-9]*" name="verifyCode" maxLength={6} value={verifyCode} onChange={handleCodeChange} placeholder="######"/>
+                                    <ul>
+                                        {Array.from({ length: 6 }).map((_, i) => (
+                                            <li key={i}>
+                                                {verifyCode[i] || '_'}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                <button type="button" onClick={() => sendVerificationEmail(email, code)} >Resent code</button>
+                            </div>
+                        </div>
+                        <p style={{ color: error === "/" ? "transparent" : "var(--color-lau-yellow)" }} className="vote-code__error">{error}</p>
+                        <div className="vote-verification__buttons">
+                            <button className="kiosk-button kiosk-button--sec" type="button" onClick={() => handleClickBack("back")} >Change Email</button>
+                            <button className="kiosk-button" type="submit">{fetcher.state === "loading" ? "Verifying..." : "Verify"}</button>
+                        </div>
+                    </form>
+                )}
+            </div>
         </div>
     );
 };
--- app/routes/kiosk/vote-theme.jsx
+++ app/routes/kiosk/vote-theme.jsx
@@ -3,6 +3,7 @@ import { getThemeVotes, addThemeVote, getVotableThemes } from "../../services/th
 import VoteVerification from "../../components/VoteVerification/VoteVerification";
 import BackButton from "../../components/BackButton/BackButton";
 import { useState } from "react";
+import "./vote-theme.css"
 
 export async function clientLoader() {
   const votableThemes = await getVotableThemes();
@@ -21,6 +22,10 @@ export async function clientAction({ request }) {
 const VoteTheme = ({ loaderData }) => {
   const { votableThemes, themeVotes } = loaderData;
 
+  const [extraInfoId, setExtraInfoId] = useState(null);
+  const [voteSelected, setVoteSelected] = useState(null)
+  const [error, setError] = useState("/")
+
   const [state, setState] = useState("email")
   const [vote, setVote] = useState("")
 
@@ -30,7 +35,11 @@ const VoteTheme = ({ loaderData }) => {
   
   const handleSubmitVote = (e) => {
     e.preventDefault()
-    setVote(e.target.theme.value)
+    if (voteSelected === null){
+      setError("Select a theme to vote")
+    } else {
+      setVote(e.target.theme.value)
+    }
   }
 
   const ClosePopup = () => {
@@ -38,16 +47,46 @@ const VoteTheme = ({ loaderData }) => {
     setVote("")
   }
 
+  const handleClickExpand = (themeId) => {
+    setExtraInfoId(prev => (prev === themeId ? null : themeId));
+  }
+
+  const handleClickTheme = () => {
+    setVoteSelected(true)
+    setError("/")
+  }
+
+  
+
   return (
     <>
-      <h1>Vote Theme</h1>
-        <BackButton/>
-        <p>Vote Theme</p>
-        <form onSubmit={handleSubmitVote}>
-          {votableThemes.map(theme => <label key={theme.id}><input type="radio" name="theme" value={theme.id} required />{theme.name}</label>)}
-          <button type="submit">Vote</button>
+      <h1 className="visually-hidden">Vote Theme</h1>
+      <div className="vote-theme__content">
+        <div className="vote-theme__head">
+          <BackButton/>
+          <h2>Vote for the next theme</h2>
+        </div>
+        <p className="vote-theme__info">Choose the theme that ignites your creativity and shapes Abby’s next immersive experience.</p>
+        <form className="vote-theme__form" onSubmit={handleSubmitVote}>
+          {votableThemes.map(theme => (
+            <div className={theme.id === extraInfoId ? "vote-theme__radio-button" : extraInfoId === null ? "vote-theme__radio-button" : "visually-hidden"}>
+              <label onClick={() => handleClickTheme(theme.id)} key={theme.id} ><input type="radio" name="theme" value={theme.id} /><span className="theme-name">{theme.name}</span><span className="square-icon" ></span></label>
+              <div className={theme.id === extraInfoId ? "" : "visually-hidden"}>
+                <img src={theme?.thumbnail} alt="Theme Thumbnail" />
+                <p>{theme.description}</p>
+              </div>
+              <button className={theme.id === extraInfoId ? "vote-theme__expand-button vote-theme__expand-button--active" : "vote-theme__expand-button"} onClick={() => handleClickExpand(theme.id)} type="button">
+                <svg width="26" height="25" viewBox="0 0 26 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16.623 24.9616L25.3284 0.452387L17.2286 0.416193L12.9244 20.2831L8.79806 0.378522L0.780826 0.342698L9.18428 24.9284L16.623 24.9616Z" fill="black" />
+                </svg>
+              </button>
+            </div>
+          ))}
+          <p className="vote-theme__error" style={{ color: error === "/" ? "transparent" : "var(--color-lau-orange)"}}>{error}</p>
+          <button className="kiosk-button kiosk-button--blue" type="submit">Vote</button>
         </form>
-      {vote != "" ? <VoteVerification themeVotes={themeVotes} state={state} changeState={changeState} themeId={vote} ClosePopup={ClosePopup} /> : null}
+      </div>
+      {vote != "" ? <VoteVerification themeVotes={themeVotes} state={state} changeState={changeState} themeId={vote} ClosePopup={ClosePopup} votableThemes={votableThemes} /> : null}
     </>
   );
 };
