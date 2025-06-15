f12df095c8c238add2e3355485bb8764adfd232e
Maxim Tampere <maxim.tampere@student.howest.be>
2025-06-15 11:08:51 +0200

    vote complete page for the kiosk html, css and working

--- app/routes/kiosk/vote-complete.jsx
+++ app/routes/kiosk/vote-complete.jsx
@@ -1,21 +1,37 @@
 import { Link } from "react-router";
-import { getThemeById } from "../../services/theme";
+import { getThemeById, getThemeVotes, getVotableThemes, getVotePercentages } from "../../services/theme";
+import "./vote-complete.css"
 
 export async function clientLoader({ params }) {
     const theme = await getThemeById(params.id);
-    return { theme };
+    const themeVotes = await getThemeVotes();
+    const votableThemes = await getVotableThemes()
+    const votePercentages = getVotePercentages(themeVotes)
+    return { theme, votableThemes, votePercentages };
 }
 
 const VoteComplete = ({ loaderData }) => {
-    const { theme } = loaderData;
+    const { theme, votableThemes, votePercentages } = loaderData;
     return (
         <>
-            <h1>Vote Complete</h1>
-            <ul>
-                <li><Link to="/kiosk/home">Go Home</Link></li>
-                <li><Link to="/kiosk/vote-art">Vote for Art</Link></li>
+            <h1 className="visually-hidden">Vote Complete</h1>
+            <div className="vote-complete__head">
+                <h2>Vote Locked in!</h2>
+                <p>You’ve cast your vote for <span>"{theme.name}"</span> Come back anytime to track its rise.</p>
+            </div>
+            <div className="vote-complete__buttons">
+                <Link className="kiosk-button kiosk-button--sec" to="/kiosk/home">Go Home</Link>
+                <p>or</p>
+                <Link className="kiosk-button" to="/kiosk/vote-art">Vote for Art</Link>
+            </div>
+            <ul className="vote-complete__percentages">
+                {votePercentages.map(item => (
+                    <li>
+                        <h3>{votableThemes.find(theme => theme.id === item.themeId).name}</h3>
+                        <p>{item.percentage}%</p>
+                    </li>
+                ))}
             </ul>
-            <p>Your vote for <b>{theme.name}</b> has been counted</p>
         </>
     );
 };
